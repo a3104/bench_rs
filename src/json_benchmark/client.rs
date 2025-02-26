96743e3d348ae09f76533d3914f574182854539c
a3104 <a3104@jaaku.org>
2025-02-26 11:30:51 +0900

    fix

--- src/json_benchmark/client.rs
+++ src/json_benchmark/client.rs
@@ -6,7 +6,10 @@ use super::BenchmarkConfig;
 pub fn build_client(config: &BenchmarkConfig) -> Result<Client, Box<dyn Error>> {
     let client_builder = Client::builder();
     let client_builder = if let Some(timeout) = config.request.timeout {
-        client_builder.timeout(std::time::Duration::from_secs(timeout))
+        client_builder
+            .timeout(std::time::Duration::from_secs(timeout))
+            .pool_idle_timeout(Some(std::time::Duration::from_secs(config.concurrent_access as u64)*10))
+            .pool_max_idle_per_host(config.concurrent_access as usize)
     } else {
         client_builder.timeout(std::time::Duration::from_secs(5)) // デフォルトのタイムアウトを5秒に設定
     };
